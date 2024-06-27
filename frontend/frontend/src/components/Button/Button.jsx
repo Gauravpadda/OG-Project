4cf2593c6831c23b336ef01c538ab3c301fb49e1
Sahib Gill <sahibgill2903@yahoo.com>
2024-06-28 03:24:57 +0530

    Button modified and Home page added

--- frontend/frontend/src/components/Button/Button.jsx
+++ frontend/frontend/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({title}) => {
     
     return (
         <div class="container">
-      <button className="w-fit h-10 border-white rounded-lg bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">{title}</button>
+      <button className="w-fit h-10 pb-8 border-white rounded-lg bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent text-2xl bg-slate-400">{title}</button>
     </div>
     );
 };
